23f407ff3ef9f399d9e8be32454ab022890a1974
Luka Milić <luka.milic@foi.hr>
2021-08-12 16:22:31 +0200

    Commi2

--- init_db.sh
+++ init_db.sh
@@ -3,7 +3,7 @@ echo "\
 CREATE USER login;
 CREATE USER local;
 CREATE USER administrator;
-GRANT SELECT(username, password, can_actually_login, is_administrator) ON TABLE users TO login;
+GRANT SELECT(username, password, is_administrator, can_actually_login) ON TABLE users TO login;
 GRANT SELECT(username, is_administrator, can_view_tables, can_send_messages, can_inject_messages,
 		can_send_queries, can_view_rules, can_view_configuration, can_view_permissions,
 		can_view_remotes, can_execute_rules, can_actually_login), UPDATE(password)
@@ -22,9 +22,9 @@ GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLE `psql -U postgres -c "SE
 				'table_constraints';" | grep -Eo 't.{16}'` TO local, administrator;
 ALTER DEFAULT PRIVILEGES GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO local,
 		administrator;
-GRANT EXECUTE ON PROCEDURE send_inject(message BYTEA, proto_id CHARACTER VARYING(10),
-		imm_addr BYTEA, CCF BOOLEAN, ACF BOOLEAN, send BOOLEAN, broadcast BOOLEAN,
-		override_implicit_rules BOOLEAN) TO local, administrator;
+GRANT EXECUTE ON PROCEDURE send_inject(send BOOLEAN, message BYTEA, proto_id TEXT, imm_addr BYTEA,
+		CCF BOOLEAN, ACF BOOLEAN, broadcast BOOLEAN, override_implicit_rules BOOLEAN) TO local,
+		administrator;
 GRANT EXECUTE ON PROCEDURE manually_execute_timed_rule(username TEXT, id INTEGER) TO local,
 		administrator;
 GRANT EXECUTE ON PROCEDURE load_store(load BOOLEAN) TO local, administrator;
@@ -37,5 +37,5 @@ INSERT INTO users(username, password, is_administrator, can_view_tables, can_sen
 		TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE);
 INSERT INTO configuration(username, forward_messages, use_internet_switch_algorithm, nsecs_id,
 		nsecs_src, trust_everyone, default_gateway) VALUES('root', TRUE, TRUE, 600, 36000, FALSE,
-		NULL);
+		E'\\\\x0000000000000000');
 " | psql -U postgres
