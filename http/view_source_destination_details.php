2ca5c39d0a2e3612c2bb443f1dcaf673577a4ef6
Luka Milić <luka.milic@foi.hr>
2021-09-09 18:57:43 +0200

    Commit2

--- http/view_source_destination_details.php
+++ http/view_source_destination_details.php
@@ -3,49 +3,55 @@ require_once 'common.php';
 if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_GET['DST'])) {
 	$sql_SRC1 = '\'t' . pg_escape_string($_GET['SRC']) . '\'';
 	$sql_SRC2 = pgescapebytea($_GET['SRC']);
-	$result1 = pgquery("SELECT TRUE FROM table_user WHERE tablename = 't{$_GET['SRC']}';");
-	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = 't{$_GET['SRC']}' AND username = '{$_SESSION['username']}';");
-	$result3 = pgquery("SELECT TRUE FROM table_user INNER JOIN users ON table_user.username = users.username WHERE table_user.tablename = 't{$_GET['SRC']}' AND NOT users.is_administrator;");
+	$sql_DST = pgescapebytea($_GET['DST']);
+	$result1 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $sql_SRC1;");
+	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $sql_SRC
+			AND username = {$_SESSION['sql_username']};");
+	$result3 = pgquery("SELECT TRUE FROM table_user INNER JOIN users
+			ON table_user.username = users.username WHERE table_user.tablename = $sql_SRC1
+			AND NOT users.is_administrator;");
 	$h_SRC = htmlspecialchars($_GET['SRC']);
 	$h_DST = htmlspecialchars($_GET['DST']);
 	$u_SRC = urlencode($_GET['SRC']);
 	$u_DST = urlencode($_GET['DST']);
-	if (!pg_fetch_row($result1) || pg_fetch_row($result2) || pg_fetch_row($result3) && $_SESSION['is_administrator'] || $_SESSION['is_root']) {
+	if (!pg_fetch_row($result1) || pg_fetch_row($result2) || pg_fetch_row($result3)
+			&& $_SESSION['is_administrator'] || $_SESSION['is_root']) {
 		if (isset($_GET['truncate'])) {
 			if (isset($_GET['confirm'])) {
-				pg_free_result(pgquery("DELETE FROM ID_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND DST = E'\\\\x{$_GET['DST']}';"));
-				echo 'Table &quot;ID_TWR&quot; truncated for SRC X&apos;', $h_SRC, '&apos; and DST X&apos;', $h_DST, "&apos;.<br/>\n";
+				pg_free_result(pgquery("DELETE FROM ID_TWR WHERE SRC = $sql_SRC2
+						AND DST = $sql_DST;"));
+				echo "Table &quot;ID_TWR&quot; truncated for SRC $html_SRC1 and DST $html_DST1.<br/>\n";
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo '<a href="?SRC=', $u_SRC, '&amp;DST=', $u_DST, "&amp;truncate&amp;confirm\">Yes</a>\n";
-				echo '<a href="?SRC=', $u_SRC, '&amp;DST=', $u_DST, "\">No</a>\n";
+				echo "<a href=\"?SRC=$u_SRC&amp;DST=$u_DST&amp;truncate&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?SRC=$u_SRC&amp;DST=$u_DST\">No</a>\n";
 				exit(0);
 			}
 		} else if (!empty($_GET['ID']) && !empty($_GET['TWR'])) {
 			if (isset($_GET['insert'])) {
-				pg_free_result(pgquery("INSERT INTO ID_TWR(SRC, DST, ID, TWR) VALUES(E'\\\\x{$_GET['SRC']}', E'\\\\x{$_GET['DST']}', {$_GET['ID']}, TIMESTAMP '{$_GET['TWR']}');"));
-				echo 'Mapping ', htmlspecialchars($_GET['ID']), ' for SRC X&apos;', $h_SRC, '&apos; and DST X&apos;', $h_DST, "&apos; inserted.<br/>\n";
+				pg_free_result(pgquery("INSERT INTO ID_TWR(SRC, DST, ID, TWR) VALUES($sql_SRC2, $sql_DST, $id, $TWR);"));
+				echo "Mapping $id for SRC $html_SRC1 and DST $html_DST1 inserted.<br/>\n";
 			} else if (!empty($_GET['key']) && isset($_GET['update'])) {
-				pg_free_result(pgquery("UPDATE ID_TWR SET(ID, TWR) = ({$_GET['ID']}, TIMESTAMP '{$_GET['TWR']}') WHERE SRC = E'\\\\x{$_GET['SRC']}' AND DST = E'\\\\x{$_GET['DST']}' AND ID = {$_GET['key']};"));
-				echo 'Mapping ', htmlspecialchars($_GET['key']), ' for SRC X&apos;', $h_SRC, '&apos; and DST X&apos;', $h_DST, "&apos; updated.<br/>\n";
+				pg_free_result(pgquery("UPDATE ID_TWR SET(ID, TWR) = ($id, $TWR) WHERE SRC = $sql_SRC2 AND DST = $sql_DST AND ID = $key;"));
+				echo "Mapping $key for SRC $html_SRC1 and DST $html_DST1 updated.<br/>\n";
 			}
 		} else if (!empty($_GET['key']) && isset($_GET['delete'])) {
 			if (isset($_GET['confirm'])) {
-				pg_free_result(pgquery("DELETE FROM ID_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND DST = E'\\\\x{$_GET['DST']}' AND ID = {$_GET['key']};"));
-				echo 'Mapping ', htmlspecialchars($_GET['key']), ' for SRC X&apos;', $h_SRC, '&apos; and DST X&apos;', $h_DST, "&apos; deleted.<br/>\n";
+				pg_free_result(pgquery("DELETE FROM ID_TWR WHERE SRC = $html_SRC2 AND DST = $html_DST AND ID = $key;"));
+				echo "Mapping $key for SRC $html_SRC1 and DST $html_DST1 deleted.<br/>\n";
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo '<a href="?SRC=', $u_SRC, '&amp;DST=', $u_DST, '&amp;key=', urlencode($_GET['key']), "&amp;delete&amp;confirm\">Yes</a>\n";
-				echo '<a href="?SRC=', $u_SRC, '&amp;DST=', $u_DST, "\">No</a>\n";
+				echo "<a href=\"?SRC=$u_SRC&amp;DST=$u_DST&amp;key=$url_key&amp;delete&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?SRC=$u_SRC&amp;DST=$u_DST\">No</a>\n";
 				exit(0);
 			}
 		}
-		$result = pgquery("SELECT ID, TWR FROM ID_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND DST = E'\\\\x{$_GET['DST']}' ORDER BY ID ASC;");
-		echo 'Viewing table &quot;ID_TWR&quot; for SRC X&apos;', $h_SRC, '&apos; and DST X&apos;', $h_DST, "&apos;.\n";
+		$result = pgquery("SELECT ID, TWR FROM ID_TWR WHERE SRC = $sql_SRC2 AND DST = $sql_DST ORDER BY ID ASC;");
+		echo "Viewing table &quot;ID_TWR&quot; for SRC $html_SRC1 and DST $html_DST1&apos;.\n";
 ?>
 		<table border="1">
 			<tbody>
@@ -64,18 +70,20 @@ if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_G
 					<td>
 						<form id="insert" action="" method="GET">
 <?php
-							echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-							echo '<input type="hidden" name="DST" value="', $h_DST, "\"/>\n";
+							echo "<input type=\"hidden\" name=\"SRC\" value=\"$h_SRC\"/>\n";
+							echo "<input type=\"hidden\" name=\"DST\" value=\"$h_DST\"/>\n";
 ?>
-							<input type="submit" name="insert" value="Insert new mapping for this SRC and this DST"/><br/>
+							<input type="submit" name="insert"
+									value="Insert new mapping for this SRC and this DST"/><br/>
 							<input type="reset" value="reset"/>
 						</form>
 						<form action="" method="GET">
 <?php
-							echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-							echo '<input type="hidden" name="DST" value="', $h_DST, "\"/>\n";
+							echo "<input type=\"hidden\" name=\"SRC\" value=\"$h_SRC\"/>\n";
+							echo "<input type=\"hidden\" name=\"DST\" value=\"$h_DST\"/>\n";
 ?>
-							<input type="submit" name="truncate" value="Truncate mappings for this SRC and this DST"/>
+							<input type="submit" name="truncate"
+									value="Truncate mappings for this SRC and this DST"/>
 						</form>
 					</td>
 				</tr>
@@ -85,33 +93,35 @@ if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_G
 					<tr>
 						<td>
 <?php
-							echo '<input form="update', $row[0], '" type="text" name="ID" value="', $row[0], "\"/>\n";
+							echo "<input form=\"update{$row[0]}\" type=\"text\" name=\"ID\" value=\"{$row[0]}\"/>\n";
 ?>
 						</td>
 						<td>
 <?php
-							echo '<input form="update', $row[0], '" type="text" name="TWR" value="', $row[1], "\"/>\n";
+							echo "<input form=\"update{$row[0]}\" type=\"text\" name=\"TWR\" value=\"'{$row[1]}\"/>\n";
 ?>
 						</td>
 						<td>
 <?php
-							echo '<form id="update', $row[0], "\" action=\"\" method=\"GET\"/>\n";
-								echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-								echo '<input type="hidden" name="DST" value="', $h_DST, "\"/>\n";
-								echo '<input type="hidden" name="key" value="', $row[0], "\"/>\n";
+							echo "<form id=\"update{$row[0]}\" action=\"\" method=\"GET\"/>\n";
+								echo "<input type=\"hidden\" name=\"SRC\" value=\"$h_SRC\"/>\n";
+								echo "<input type=\"hidden\" name=\"DST\" value=\"$h_DST\"/>\n";
+								echo "<input type=\"hidden\" name=\"key\" value=\"{$row[0]}\"/>\n";
 ?>
-								<input type="submit" name="update" value="Update this mapping for this SRC and this DST\"/><br/>
+								<input type="submit" name="update"
+										value="Update this mapping for this SRC and this DST\"/><br/>
 								<input type="reset" value="reset"/>
 <?php
 							echo "</form>\n";
 ?>
 							<form action="" method="GET">
 <?php
-								echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-								echo '<input type="hidden" name="DST" value="', $h_DST, "\"/>\n";
-								echo '<input type="hidden" name="key" value="', $row[0], "/>\n";
+								echo "<input type=\"hidden\" name=\"SRC\" value=\"{$h_SRC}\"/>\n";
+								echo "<input type=\"hidden\" name=\"DST\" value=\"{$h_DST}\"/>\n";
+								echo "<input type=\"hidden\" name=\"key\" value=\"{$row[0]}/>\n";
 ?>
-								<input type="submit" name="delete" value="Delete this mapping for this SRC and this DST"/>
+								<input type="submit" name="delete"
+										value="Delete this mapping for this SRC and this DST"/>
 							</form>
 						</td>
 					</tr>
--- http/view_source_protocol_details.php
+++ http/view_source_protocol_details.php
@@ -1,49 +1,58 @@
 <?php
 require_once 'common.php';
 if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_GET['proto'])) {
-	$result1 = pgquery("SELECT TRUE FROM table_user WHERE tablename = 't{$_GET['SRC']}';");
-	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = 't{$_GET['SRC']}' AND username = '{$_SESSION['username']}';");
-	$result3 = pgquery("SELECT TRUE FROM table_user INNER JOIN users ON table_user.username = users.username WHERE tablename = 't{$_GET['SRC']}' AND NOT users.is_administrator;");
-	$h_SRC = htmlspecialchars($_GET['SRC']);
-	$h_proto = htmlspecialchars($_GET['proto']);
-	$u_SRC = urlencode($_GET['SRC']);
-	$u_proto = urlencode($_GET['proto']);
-	if (!pg_fetch_row($result1) || pg_fetch_row($result2) || pg_fetch_row($result3) && $_SESSION['is_administrator'] || $_SESSION['is_root']) {
+	$sql_SRC1 = '\'t' . pg_escape_string($_GET['SRC']);
+	$sql_SRC2 = pgescapebytea($_GET['SRC']);
+	$sql_proto = pg_escape_literal($_GET['proto']);
+	$html_SRC2 = "X&apos;$html_SRC1&apos;";
+	$html_proto2 = "'$html_proto1'";
+	$result1 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $sql_SRC1;");
+	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $sql_SRC2
+			AND username = {$_SESSION['sql_username']};");
+	$result3 = pgquery("SELECT TRUE FROM table_user INNER JOIN users
+			ON table_user.username = users.username WHERE tablename = $sql_SRC1
+			AND NOT users.is_administrator;");
+	$html_SRC1 = htmlspecialchars($_GET['SRC']);
+	$html_proto1 = htmlspecialchars($_GET['proto']);
+	$url_SRC2 = urlencode($_GET['SRC']);
+	$url_proto2 = urlencode($_GET['proto']);
+	if (!pg_fetch_row($result1) || pg_fetch_row($result2) || pg_fetch_row($result3)
+			&& $_SESSION['is_administrator'] || $_SESSION['is_root']) {
 		if (isset($_GET['truncate'])) {
 			if (isset($_GET['confirm'])) {
-				pg_free_result(pgquery("DELETE FROM iSRC_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND proto = '{$_GET['proto']}';"));
-				echo 'Table &quot;iSRC_TWR&quot; truncated for SRC X&apos;', $h_SRC, '&apos; and proto &apos;', $h_proto, "&apos;.<br/>\n";
+				pg_free_result(pgquery("DELETE FROM iSRC_TWR WHERE SRC = $sql_SRC2 AND proto = $sql_proto;"));
+				echo "Table &quot;iSRC_TWR&quot; truncated for SRC $html_SRC1 and proto $html_proto1&apos;.<br/>\n";
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo '<a href="?SRC=', $u_SRC, '&amp;proto=', $u_proto, "&amp;truncate&amp;confirm\">Yes</a>\n";
-				echo '<a href="?SRC=', $u_SRC, '&amp;proto=', $u_proto, "\">No</a>\n";
+				echo "<a href=\"?SRC=$url_SRC&amp;proto=$url_proto&amp;truncate&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?SRC=$u_SRC&amp;proto=$u_proto\">No</a>\n";
 				exit(0);
 			}
 		} else if (!empty($_GET['imm_SRC']) && !empty($_GET['TWR'])) {
 			if (isset($_GET['insert'])) {
-				pg_free_result(pgquery("INSERT INTO iSRC_TWR(SRC, proto, imm_SRC, TWR) VALUES(E'\\\\x{$_GET['SRC']}', (SELECT proto FROM proto WHERE name = '{$_GET['proto']}'), E'\\\\x{$_GET['imm_SRC']}', TIMESTAMP '{$_GET['TWR']}');"));
-				echo 'Mapping X&apos;', htmlspecialchars($_GET['imm_SRC']), '&apos; for SRC X&apos;', $h_SRC, '&apos; and proto &apos;', $h_proto, "&apos; inserted.<br/>\n";
+				pg_free_result(pgquery("INSERT INTO iSRC_TWR(SRC, proto, imm_SRC, TWR) VALUES($sql_SRC2, (SELECT proto FROM proto WHERE name = $sql_proto), $sql_imm_SRC, $sql_TWR);"));
+				echo "Mapping $html_imm_SRC for SRC $html_SRC1 and proto $html_proto1 inserted.<br/>\n";
 			} else if (!empty($_GET['key']) && isset($_GET['update'])) {
-				pg_free_result(pgquery("UPDATE iSRC_TWR SET (imm_SRC, TWR) = (E'\\\\x{$_GET['imm_SRC']}', TIMESTAMP '{$_GET['TWR']}') WHERE SRC = E'\\\\x{$_GET['SRC']}' AND proto = (SELECT proto FROM proto WHERE name = '{$_GET['proto']}') AND imm_SRC = E'\\\\x{$_GET['imm_SRC']}';"));
-				echo 'Mapping X&apos;', htmlspecialchars($_GET['key']), '&apos; for SRC X&apos;', $h_SRC, '&apos; and proto &apos;', $h_proto, "&apos; updated.<br/>\n";
+				pg_free_result(pgquery("UPDATE iSRC_TWR SET (imm_SRC, TWR) = ($sql_imm_SRC, $sql_TWR) WHERE SRC = $sql_SRC2 AND proto = (SELECT proto FROM proto WHERE name = $sql_proto) AND imm_SRC = $sql_imm_SRC;"));
+				echo "Mapping $html_key for SRC $html_SRC1 and proto $html_proto1 updated.<br/>\n";
 			}
 		} else if (!empty($_GET['key']) && isset($_GET['delete'])) {
 			if (isset($_GET['confirm'])) {
-				pg_free_result(pgquery("DELETE FROM iSRC_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND proto = (SELECT proto FROM proto WHERE name = '{$_GET['proto']}') AND imm_SRC = E'\\\\x{$_GET['key']}';"));
-				echo 'Mapping X&apos;', htmlspecialchars($_GET['key']), '&apos; for SRC X&apos;', $h_SRC, '&apos; and proto &apos;', $h_proto, "&apos; deleted.<br/>\n";
+				pg_free_result(pgquery("DELETE FROM iSRC_TWR WHERE SRC = $sql_SRC2 AND proto = (SELECT proto FROM proto WHERE name = $sql_proto) AND imm_SRC = $sql_key;"));
+				echo "Mapping $html_key for SRC $html_SRC1 and proto $html_proto deleted.<br/>\n";
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo '<a href="?SRC=', $u_SRC, '&amp;proto=', $u_proto, '&amp;key=', urlencode($_GET['key']), "&amp;delete&amp;confirm\">Yes</a>\n";
-				echo '<a href="?SRC=', $u_SRC, '&amp;proto=', $u_proto, "\">No</a>\n";
+				echo "<a href=\"?SRC=$url_SRC&amp;proto=$url_proto&amp;key=$url_key&amp;delete&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?SRC=$url_SRC&amp;proto=$u_proto\">No</a>\n";
 				exit(0);
 			}
 		}
-		$result = pgquery("SELECT imm_SRC, TWR FROM iSRC_TWR WHERE SRC = E'\\\\x{$_GET['SRC']}' AND proto = '{$_GET['proto']}' ORDER BY imm_SRC ASC;");
-		echo 'Viewing table &quot;iSRC_TWR&quot; for SRC X&apos;', $h_SRC, '&apos; and proto &apos;', $h_proto, "&apos;.\n";
+		$result = pgquery("SELECT imm_SRC, TWR FROM iSRC_TWR WHERE SRC = $sql_SRC2 AND proto = $sql_proto ORDER BY imm_SRC ASC;");
+		echo "Viewing table &quot;iSRC_TWR&quot; for SRC $html_SRC1 and proto $html_proto.\n";
 ?>
 		<table border="1">
 			<tbody>
@@ -62,18 +71,20 @@ if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_G
 					<td>
 						<form id="insert" action="" method="GET">
 <?php
-							echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-							echo '<input type="hidden" name="proto" value="', $h_proto, "\"/>\n";
+							echo "<input type=\"hidden\" name=\"SRC\" value=\"$html_SRC1\"/>\n";
+							echo "<input type=\"hidden\" name=\"proto\" value=\"$html_proto1\"/>\n";
 ?>
-							<input type="submit" name="insert" value="Insert new mapping for this SRC and this proto"/><br/>
+							<input type="submit" name="insert"
+									value="Insert new mapping for this SRC and this proto"/><br/>
 							<input type="reset" value="reset"/>
 						</form>
 						<form action="" method="GET">
 <?php
-							echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-							echo '<input type="hidden" name="proto" value="', $h_proto, "\"/>\n";
+							echo "<input type=\"hidden\" name=\"SRC\" value=\"$html_SRC1\"/>\n";
+							echo "<input type=\"hidden\" name=\"proto\" value=\"$html_proto1\"/>\n";
 ?>
-							<input type="submit" name="truncate" value="Truncate mappings for this SRC and this proto"/>
+							<input type="submit" name="truncate"
+									value="Truncate mappings for this SRC and this proto"/>
 						</form>
 					</td>
 				</tr>
@@ -84,33 +95,35 @@ if (checkAuthorization(10, 'view remotes') && !empty($_GET['SRC']) && !empty($_G
 						<td>
 <?php
 							$str = substr($row[0], 2);
-							echo '<input form="update', $str, '" type="text" name="imm_SRC" value="', $str, "\"/>\n";
+							echo "<input form=\"update$str\" type=\"text\" name=\"imm_SRC\" value=\"$str\"/>\n";
 ?>
 						</td>
 						<td>
 <?php
-							echo '<input form="update', $str, '" type="text" name="TWR" value="', $row[1], "\"/>\n";
+							echo "<input form=\"update$str\" type=\"text\" name=\"TWR\" value=\"{$row[1]}\"/>\n";
 ?>
 						</td>
 						<td>
 <?php
-							echo '<form id="update', $str, "\" action=\"\" method=\"GET\">\n";
-								echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-								echo '<input type="hidden" name="proto" value="', $h_proto, "\"/>\n";
-								echo '<input type="hidden" name="key" value="', $str, "\"/>\n";
+							echo "<form id=\"update$str\" action=\"\" method=\"GET\">\n";
+								echo "<input type=\"hidden\" name=\"SRC\" value=\"$html_SRC1\"/>\n";
+								echo "<input type=\"hidden\" name=\"proto\" value=\"$html_proto1\"/>\n";
+								echo "<input type=\"hidden\" name=\"key\" value=\"$str\"/>\n";
 ?>
-								<input type="submit" name="update" value="Update this mapping for this SRC and this proto"/><br/>
+								<input type="submit" name="update"
+										value="Update this mapping for this SRC and this proto"/><br/>
 								<input type="reset" value="reset"/>
 <?php
 							echo "</form>\n";
 ?>
 							<form action="" method="GET">
 <?php
-								echo '<input type="hidden" name="SRC" value="', $h_SRC, "\"/>\n";
-								echo '<input type="hidden" name="proto" value="', $h_proto, "\"/>\n";
-								echo '<input type="hidden" name="key" value="', $str, "\"/>\n";
+								echo "<input type=\"hidden\" name=\"SRC\" value=\"{$h_SRC}\"/>\n";
+								echo "<input type=\"hidden\" name=\"proto\" value=\"{$h_proto}\"/>\n";
+								echo "<input type=\"hidden\" name=\"key\" value=\"$str\"/>\n";
 ?>
-								<input type="submit" name="delete" value="Delete this mapping for this SRC and this proto"/>
+								<input type="submit" name="delete"
+										value="Delete this mapping for this SRC and this proto"/>
 							</form>
 						</td>
 					</tr>
