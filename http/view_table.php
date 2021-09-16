d8dad5db4a1733ddf762ddc68748b64e4a3e6692
Luka Milić <luka.milic@foi.hr>
2021-09-16 22:22:05 +0200

    Commit2

--- http/view_table.php
+++ http/view_table.php
@@ -44,84 +44,90 @@ function standardToPostgresqlInput($data, $oid) {
 	case 25://TEXT
 		return $data;
 	case 17://BYTEA
-		return 'E\'\\\\x' . substr($data, 2);
+		return '\'\\x' . substr($data, 2);
 	default://16//BOOLEAN
 		return $data == 'UNKNOWN' ? 'NULL' : $data;
 	}
 }
 
 if (checkAuthorization(3, 'view tables') && !empty($_GET['tablename'])) {
-	$sql_tablename1 = pg_escape_literal($_GET['tablename']);
-	$sql_tablename2 = pg_escape_identifier($_GET['tablename']);
-	$html_tablename2 = "\"$html_tablename1\"";
-	$result1 = pgquery("SELECT username FROM table_user WHERE tablename = $sql_tablename1;");
-	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $sql_tablename1
-			AND username = {$_SESSION['sql_username']};");
+	$s1tablename = pg_escape_literal($_GET['tablename']);
+	$s2tablename = pg_escape_identifier($_GET['tablename']);
+	$h1tablename = htmlspecialchars($_GET['tablename']);
+	$h2tablename = "&quot;$html_tablename1&quot;";
+	$u_tablename = urlencode($_GET['tablename']);
+	$result1 = pgquery("SELECT username FROM table_user WHERE tablename = $s1tablename;");
+	$result2 = pgquery("SELECT TRUE FROM table_user WHERE tablename = $s1tablename
+			AND username = {$_SESSION['s_username']};");
 	$result3 = pgquery("SELECT TRUE FROM table_user INNER JOIN users
 			ON table_user.username = users.username
-			WHERE table_user.tablename = $sql_tablename1 AND NOT users.is_administrator;");
-	$html_tablename1 = htmlspecialchars($_GET['tablename']);
-	$url_tablename = urlencode($_GET['tablename']);
+			WHERE table_user.tablename = $s1tablename AND NOT users.is_administrator;");
 	$row = pg_fetch_row($result1);
 	if (!$row || $row[0] === null || pg_fetch_row($result2) || pg_fetch_row($result3)
 			&& $_SESSION['is_administrator'] || $_SESSION['is_root']) {
 		if (isset($_GET['truncate'])) {
 			if (isset($_GET['confirm'])) {
-				$result = pgquery("TRUNCATE TABLE $sql_tablename2;");
-				echo "Table $html_tablename2 truncated.<br/>\n";
+				$result = pgquery("TRUNCATE TABLE $s2tablename;");
+				echo "Table $h2tablename truncated.<br/>\n";
 				pg_free_result($result);
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo "<a href=\"?tablename=$url_tablename&amp;truncate&amp;confirm\">Yes</a>\n";
-				echo "<a href=\"?tablename=$url_tablename\">No</a>";
+				echo "<a href=\"?tablename=$u_tablename&amp;truncate&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?tablename=$u_tablename\">No</a>";
 				exit(0);
 			}
 		} else if (!empty($_GET['t'])) {
 			if (isset($_GET['insert'])) {
-				$result = pgquery("SELECT * FROM $sql_tablename2 WHERE FALSE;");
-				$query = "INSERT INTO $sql_tablename2(";
+				$t = 'TIMESTAMP &apos;' . htmlspecialchars($_GET['t']) . '&apos;';
+				$result = pgquery("SELECT * FROM $s2tablename WHERE FALSE;");
+				$query = "INSERT INTO $s2tablename(";
 				for ($i = 0, $j = pg_num_fields($result); $i < $j; $i++) {
 					$query .= pg_field_name($result, $i) . ', ';
 				}
 				$query = substr($query, 0, -2) . ') VALUES(';
 				for ($i = 0; $i < $j; $i++) {
-					$query .= standardToPostgresqlInput($_GET[pg_field_name($result, $i)], pg_field_type_oid($result, $i)) . ', ';
+					$query .= standardToPostgresqlInput($_GET[pg_field_name($result, $i)],
+							pg_field_type_oid($result, $i)) . ', ';
 				}
 				$result = pgquery(substr($query, 0, -2) . ');');
-				echo "Row $timestamp inserted.<br/>\n";
+				echo "Row $t inserted.<br/>\n";
 				pg_free_result($result);
 			} else if (!empty($_GET['key']) && isset($_GET['update'])) {
-				$result = pgquery("SELECT * FROM $sql_tablename2 WHERE FALSE;");
-				$query = "UPDATE $sql_tablename2 SET (";
+				$s_key = 'TIMESTAMP ' . pg_escape_literal($_GET['key']);
+				$h_key = 'TIMESTAMP &apos;' . htmlspecialchars($_GET['key']) . '&apos;';
+				$result = pgquery("SELECT * FROM $s2tablename WHERE FALSE;");
+				$query = "UPDATE $s2tablename SET (";
 				for ($i = 0, $j = pg_num_fields($result); $i < $j; $i++) {
 					$query .= pg_field_name($result, $i) . ', ';
 				}
 				$query = substr($query, 0, -2) . ') = ROW (';
 				for ($i = 0; $i < $j; $i++) {
-					$query .= standardToPostgresqlInput($_GET[pg_field_name($result, $i)], pg_field_type_oid($result, $i)) . ', ';
+					$query .= standardToPostgresqlInput($_GET[pg_field_name($result, $i)],
+							pg_field_type_oid($result, $i)) . ', ';
 				}
-				$result = pgquery(substr($query, 0, -2) . ") WHERE t = $sql_key;");
-				echo "Row $html_key updated.<br/>\n";
+				$result = pgquery(substr($query, 0, -2) . ") WHERE t = $s_key;");
+				echo "Row $h_key updated.<br/>\n";
 				pg_free_result($result);
 			}
 		} else if (!empty($_GET['key']) && isset($_GET['delete'])) {
 			if (isset($_GET['confirm'])) {
-				$result = pgquery("DELETE FROM $sql_tablename2 WHERE t = $sql_key;");
-				echo "Row $html_key deleted.<br/>\n";
+				$result = pgquery("DELETE FROM $s2tablename WHERE t = $s_key;");
+				echo "Row $h_key deleted.<br/>\n";
 				pg_free_result($result);
 			} else {
 ?>
 				Are you sure?
 <?php
-				echo "<a href=\"?tablename=$url_tablename&amp;key=$url_key&amp;delete&amp;confirm\">Yes</a>\n";
-				echo "<a href=\"?tablename=$url_tablename\">No</a>";
+				echo '<a href="?tablename=',
+						"$u_tablename&amp;key=$u_key&amp;delete&amp;confirm\">Yes</a>\n";
+				echo "<a href=\"?tablename=$u_tablename\">No</a>";
 				exit(0);
 			}
 		}
-		$result = pgquery("TABLE $sql_tablename2 ORDER BY t DESC;");
-		echo "Viewing table $html_tablename2, newest first.\n";
+		$result = pgquery("TABLE $s2tablename ORDER BY t DESC;");
+		echo "Viewing table $h2tablename, newest first.\n";
 ?>
 		<table border="1">
 			<tbody>
@@ -150,14 +156,16 @@ if (checkAuthorization(3, 'view tables') && !empty($_GET['tablename'])) {
 					<td>
 						<form id="insert" action="" method="GET">
 <?php
-							echo "<input type=\"hidden\" name=\"tablename\" value=\"$html_tablename1\"/>\n";
+							echo "<input type=\"hidden\" name=\"tablename\"
+									value=\"$h1tablename\"/>\n";
 ?>
 							<input type="submit" name="insert" value="INSERT"/><br/>
 							<input type="reset" value="reset"/>
 						</form>
 						<form action="" method="GET">
 <?php
-							echo "<input type=\"hidden\" name=\"tablename\" value=\"$html_tablename1\"/>\n";
+							echo "<input type=\"hidden\" name=\"tablename\"
+									value=\"$h1tablename\"/>\n";
 ?>
 							<input type="submit" name="truncate" value="TRUNCATE"/>
 						</form>
@@ -187,8 +195,9 @@ if (checkAuthorization(3, 'view tables') && !empty($_GET['tablename'])) {
 							echo "<form id=\"update{$row[$t]}\" action=\"\" method=\"GET\">\n";
 								echo '<input type="hidden" name="key" value="TIMESTAMP ',
 										pg_field_type_oid($result, $t) == 1184
-										? 'WITH TIME ZONE ' : '', '&apos;', $row[$t], "&apos;\"/>\n";
-								echo "<input type=\"hidden\" name=\"tablename\" value=\"$html_tablename1\"/>\n";
+										? 'WITH TIME ZONE ' : '', "&apos;{$row[$t]}&apos;\"/>\n";
+								echo "<input type=\"hidden\" name=\"tablename\"
+										value=\"$h1tablename\"/>\n";
 ?>
 								<input type="submit" name="update" value="UPDATE"/><br/>
 								<input type="reset" value="reset"/>
@@ -199,8 +208,9 @@ if (checkAuthorization(3, 'view tables') && !empty($_GET['tablename'])) {
 <?php
 								echo '<input type="hidden" name="key" value="TIMESTAMP ',
 										pg_field_type_oid($result, $t) == 1184
-										? 'WITH TIME ZONE ' : '', '&apos;', $row[$t], "&apos;\"/>\n";
-								echo "<input type=\"hidden\" name=\"tablename\" value=\"$html_tablename1\"/>\n";
+										? 'WITH TIME ZONE ' : '', "&apos;{$row[$t]}&apos;\"/>\n";
+								echo "<input type=\"hidden\" name=\"tablename\"
+										value=\"$h1tablename\"/>\n";
 ?>
 								<input type="submit" name="delete" value="DELETE"/>
 							</form>
--- http/view_users.php
+++ http/view_users.php
@@ -16,6 +16,8 @@ if (isset($_GET['truncate']) && $_SESSION['is_root']) {
 	}
 } else if (($_SESSION['is_administrator'] && !isset($_POST['is_administrator'])
 			|| $_SESSION['is_root']) && isset($_POST['insert']) && isset($_POST['username'])) {
+	$s_username = pg_escape_literal($_POST['username']);
+	$h_username = '&apos;' . htmlspecialchars($_POST['username']) . '&apos;';
 	$query = 'INSERT INTO users(username, password';
 	$fields = array('is_administrator', 'can_view_tables', 'can_send_messages',
 			'can_inject_messages', 'can_send_queries', 'can_view_rules', 'can_view_configuration',
@@ -23,23 +25,25 @@ if (isset($_GET['truncate']) && $_SESSION['is_root']) {
 	for ($i = 0; $i < 11; $i++) {
 		$query .= ", {$fields[$i]}";
 	}
-	$query .= ") VALUES('{$_POST['username']}', '"
-			. password_hash($_POST['password'], PASSWORD_DEFAULT) . '\'';
+	$query .= ") VALUES($s_username']}, '" . password_hash($_POST['password'], PASSWORD_DEFAULT) .
+			'\'';
 	for ($i = 0; $i < 11; $i++) {
 		$query .= ', ' . pgescapebool($_POST[$fields[$i]]);
 	}
 	pg_free_result(pgquery($query . ');'));
 	pg_free_result(pgquery("INSERT INTO configuration(username, forward_messages,
 			use_internet_switch_algorithm, nsecs_id, nsecs_src, trust_everyone, default_gateway)
-			SELECT $sql_username, forward_messages, use_internet_switch_algorithm, nsecs_id,
+			SELECT $s_username, forward_messages, use_internet_switch_algorithm, nsecs_id,
 			nsecs_src, trust_everyone, default_gateway FROM configuration
 			WHERE username = 'root';"));
-	echo "User $html_username inserted.<br/>\n";
+	echo "User $h_username inserted.<br/>\n";
 } else if (isset($_POST['update1']) && isset($_POST['username'])) {
-	$result = pgquery("SELECT TRUE FROM users WHERE username = $sql_username
+	$s_username = pg_escape_literal($_POST['username']);
+	$h_username = '&apos;' . htmlspecialchars($_POST['username']) . '&apos;';
+	$result = pgquery("SELECT TRUE FROM users WHERE username = $s_username
 			AND NOT is_administrator;");
-	if ($_SESSION['is_administrator'] && !isset($_POST['is_administrator'])
-			&& pg_fetch_row($result) || $_SESSION['is_root']) {
+	if ($_SESSION['is_administrator'] && !isset($_POST['is_administrator']) && pg_fetch_row($result)
+			|| $_SESSION['is_root']) {
 		$query = 'UPDATE users SET (' . (!empty($_POST['password']) ? 'password, ' : '');
 		$fields = array('is_administrator', 'can_view_tables', 'can_send_messages',
 				'can_inject_messages', 'can_send_queries', 'can_view_rules',
@@ -53,21 +57,24 @@ if (isset($_GET['truncate']) && $_SESSION['is_root']) {
 		for ($i = $_SESSION['is_root'] ? 0 : 1; $i < 11; $i++) {
 			$query .= ', ' . pgescapebool($_POST[$fields[$i]]);
 		}
-		pg_free_result(pgquery(substr($query, 0, -2) . ") WHERE username = $sql_username;"));
-		echo "User $html_username updated.<br/>\n";
+		pg_free_result(pgquery(substr($query, 0, -2) . ") WHERE username = $s_username;"));
+		echo "User $h_username updated.<br/>\n";
 	}
 	pg_free_result($result);
 } else if (isset($_GET['delete']) && isset($_GET['key'])) {
-	$result = pgquery("SELECT TRUE FROM users WHERE username = $sql_key AND NOT is_administrator;");
+	$s_key = pg_escape_literal($_GET['key']);
+	$h_key = '&apos;' . htmlspecialchars($_GET['key']) . '&apos;';
+	$u_key = urlencode($_GET['key']);
+	$result = pgquery("SELECT TRUE FROM users WHERE username = $s_key AND NOT is_administrator;");
 	if ($_SESSION['is_administrator'] && pg_fetch_row($result) || $_SESSION['is_root']) {
 		if (isset($_GET['confirm'])) {
-			pg_free_result(pgquery("DELETE FROM users WHERE username = $sql_key;"));
-			echo "User $html_key deleted.<br/>\n";
+			pg_free_result(pgquery("DELETE FROM users WHERE username = $s_key;"));
+			echo "User $h_key deleted.<br/>\n";
 		} else {
 ?>
 			Are you sure?
 <?php
-			echo "<a href=\"?key=$url_key&amp;delete&amp;confirm\">Yes</a>\n";
+			echo "<a href=\"?key=$u_key&amp;delete&amp;confirm\">Yes</a>\n";
 ?>
 			<a href="?">No</a>
 <?php
@@ -77,13 +84,13 @@ if (isset($_GET['truncate']) && $_SESSION['is_root']) {
 	pg_free_result($result);
 } else if (isset($_POST['update2']) && isset($_POST['password'])) {
 	pg_free_result(pgquery('UPDATE users SET password = \'' . password_hash($_POST['password'],
-			PASSWORD_DEFAULT) . "' WHERE username = {$_SESSION['sql_username']};"));
-	echo "Password updated - for username $html_username.<br/>\n";
+			PASSWORD_DEFAULT) . "' WHERE username = {$_SESSION['s_username']};"));
+	echo "Password updated - for username $h2username.<br/>\n";
 }
 $result1 = pgquery("SELECT username, TRUE, is_administrator, can_view_tables, can_send_messages,
 		can_inject_messages, can_send_queries, can_view_rules, can_view_configuration,
 		can_view_permissions, can_view_remotes, can_execute_rules, can_actually_login FROM users
-		WHERE username = {$_SESSION['sql_username']};");
+		WHERE username = {$_SESSION['s_username']};");
 if ($_SESSION['is_root']) {
 	$result2 = pgquery("SELECT * FROM users WHERE username <> 'root'
 			ORDER BY is_administrator DESC, username ASC;");
@@ -95,10 +102,11 @@ if ($_SESSION['is_root']) {
 			can_send_messages, can_inject_messages, can_send_queries, can_view_rules,
 			can_view_configuration, can_view_permissions, can_view_remotes, can_execute_rules,
 			can_actually_login FROM users WHERE NOT is_administrator
-			AND username <> {$_SESSION['sql_username']} ORDER BY username ASC;");
-	echo "Viewing table &quot;users&quot; for username {$_SESSION['html_username']} and non-administrators.\n";
+			AND username <> {$_SESSION['s_username']} ORDER BY username ASC;");
+	echo "Viewing table &quot;users&quot; for username {$_SESSION['h2username']}
+			and non-administrators.\n";
 } else {
-	echo "Viewing table &quot;users&quot; for username {$_SESSION['html_username']}.\n";
+	echo "Viewing table &quot;users&quot; for username {$_SESSION['h2username']}.\n";
 }
 ?>
 <table border="1">
@@ -195,7 +203,8 @@ if ($_SESSION['is_root']) {
 			<td>
 <?php
 				$row = pg_fetch_row($result1);
-				echo '<input type="text" value="', htmlspecialchars($row[0]), "\" disabled=\"disabled\"/>\n";
+				echo '<input type="text" value="', htmlspecialchars($row[0]),
+						"\" disabled=\"disabled\"/>\n";
 ?>
 			</td>
 			<td>
@@ -228,12 +237,14 @@ if ($_SESSION['is_root']) {
 <?php
 						$username = htmlspecialchars($row[0]);
 						echo "<input type=\"text\" value=\"$username\" disabled=\"disabled\"/>\n";
-						echo "<input form=\"update1_$username\" type=\"hidden\" name=\"username\" value=\"$username\"/>\n";
+						echo "<input form=\"update1_$username\" type=\"hidden\" name=\"username\"
+								value=\"$username\"/>\n";
 ?>
 					</td>
 					<td>
 <?php
-						echo "<input form=\"update1_$username\" type=\"text\" name=\"password\"/>\n";
+						echo "<input form=\"update1_$username\" type=\"text\"
+								name=\"password\"/>\n";
 ?>
 					</td>
 <?php
@@ -252,7 +263,7 @@ if ($_SESSION['is_root']) {
 ?>
 					<td>
 <?php
-						echo '<form id="update1_', $username, "\" action=\"\" method=\"POST\">\n";
+						echo "<form id=\"update1_$username\" action=\"\" method=\"POST\">\n";
 ?>
 							<input type="submit" name="update1" value="UPDATE"/><br/>
 							<input type="reset" value="reset"/>
@@ -261,7 +272,7 @@ if ($_SESSION['is_root']) {
 ?>
 						<form action="" method="GET">
 <?php
-							echo '<input type="hidden" name="key" value="', $username, "\"/>\n";
+							echo "<input type=\"hidden\" name=\"key\" value=\"$username\"/>\n";
 ?>
 							<input type="submit" name="delete" value="DELETE"/>
 						</form>
