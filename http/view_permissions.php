62d59fe536b767184bebe5c75f45cfc0def06eca
Luka Milić <luka.milic@foi.hr>
2021-12-12 22:39:46 +0100

    Commit2

--- http/view_permissions.php
+++ http/view_permissions.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'common.php';
-$can_view_permissions = checkAuthorization(10, 'view permissions');
-$can_edit_permissions = checkAuthorization(11, 'edit permissions');
+$can_view_permissions = checkAuthorization(12, 'view permissions');
+$can_edit_permissions = checkAuthorization(13, 'edit permissions');
 if ($can_edit_permissions) {
 	if (isset($_GET['truncate']) && $_SESSION['is_root']) {
 		if (isset($_GET['confirm'])) {
-			pg_free_result(pgquery('TRUNCATE TABLE table_user;'));
+			pgquery('TRUNCATE TABLE table_user;');
 			echo "Table &quot;table_user&quot; truncated.<br/>\n";
 		} else {
 ?>
@@ -20,18 +20,15 @@ if ($can_edit_permissions) {
 		$h_tablename = '&apos;' . htmlspecialchars($_GET['tablename']) . '&apos;';
 		$s_username = pg_escape_literal($_GET['username']);
 		$h_username = '&apos;' . htmlspecialchars($_GET['username']) . '&apos;';
-		$username_is_administrator = is_administrator($s_username);
-		$username_is_user = $_GET['username'] == $_SESSION['username'];
 		$is_read_only = isset($_GET['is_read_only']);
-		$s_is_readonly = pgescapebool($_GET['is_read_only']);
+		$s_is_read_only = pgescapebool($_GET['is_read_only']);
 		$tablename_owner = find_owner($s_tablename);
 		$tablename_owner_is_administrator = is_administrator($tablename_owner);
-		$tablename_owner_is_user = $tablename_owner == $_SESSION['username'];
+		$tablename_owner_is_user_or_public = $tablename_owner == $_SESSION['username']
+				|| $tablename_owner == 'public';
 		if (isset($_GET['insert'])) {
-			if ($is_read_only && ($username_is_user && $tablename_owner == 'public'
-					|| $_SESSION['is_administrator'] && ($username_is_user
-					|| !$username_is_administrator) && ($tablename_owner_is_user
-					|| !$tablename_owner_is_administrator) || $_SESSION['is_root'])) {
+			if ($tablename_owner_is_user_or_public || $_SESSION['is_administrator']
+					&& !$tablename_owner_is_administrator || $_SESSION['is_root']) {
 				pgquery("INSERT INTO table_user(tablename, username, is_read_only)
 						VALUES($s_tablename, $s_username, FALSE);");
 				echo "Row ($h_tablename, $h_username, FALSE) inserted.<br/>\n";
@@ -40,19 +37,16 @@ if ($can_edit_permissions) {
 			$s_key1 = pg_escape_literal($_GET['key1']);
 			$h_key1 = '&apos;' . htmlspecialchars($_GET['key1']) . '&apos;';
 			$key1_owner = find_owner($s_key1);
+			$key1_owner_is_administrator = is_administrator($key1_owner);
+			$key1_owner_is_user_or_public = $key1_owner == $_SESSION['username']
+					|| $key1_owner == 'public';
 			$s_key2 = pg_escape_literal($_GET['key2']);
 			$h_key2 = '&apos;' . htmlspecialchars($_GET['key2']) . '&apos;';
-			$key2_is_administrator = is_administrator($s_key2);
-			$key2_is_user = $_GET['key2'] == $_SESSION['username'];
-			$key3 = isset($_GET['key3']);
-			if ($is_read_only == $key3 && ($key2_is_user && $username_is_user
-					&& $tablename_owner == 'public' && $key1_owner == 'public'
-					|| $_SESSION['is_administrator'] && ($key2_is_user
-					&& !$username_is_administrator || !$key2_is_administrator && $username_is_user
-					|| !$key2_is_administrator && !$username_is_administrator)
-					&& ($tablename_owner_is_user || !$tablename_owner_is_administrator)
-					&& ($key1_owner == $_SESSION['username'] || !is_administrator($key1_owner))
-					|| $_SESSION['is_root'])) {
+			if (($is_read_only || $_GET['key1'] == $_GET['tablename'])
+					&& $key1_owner_is_user_or_public && $tablename_owner_is_user_or_public
+					|| $_SESSION['is_administrator'] && ($key1_owner_is_user_or_public
+					|| !$key1_owner_is_administrator) && ($tablename_owner_is_user_or_public
+					|| !$tablename_owner_is_administrator) || $_SESSION['is_root']) {
 				pgquery("UPDATE table_user SET (tablename, username) = ($s_tablename, $s_username)
 						WHERE tablename = $s_key1 AND username = $s_key2;");
 				echo "Row ($h_key1, $h_key2, $s_is_read_only)
@@ -67,15 +61,13 @@ if ($can_edit_permissions) {
 		$s_key2 = pg_escape_literal($_GET['key2']);
 		$h_key2 = '&apos;' . htmlspecialchars($_GET['key2']) . '&apos;';
 		$u_key2 = urlencode($_GET['key2']);
-		$key2_is_user = $_GET['key2'] == $_SESSION['username'];
-		$key3 = isset($_GET['key3']);
-		if (!$key3 && (($key2_is_user && $key1_owner == 'public' || $_SESSION['is_administrator']
-				&& ($key2_is_user || !is_administrator($s_key2))
-				&& ($key1_owner == $_SESSION['username'] || !is_administrator($key1_owner))
-				|| $_SESSION['is_root']) && isset($_GET['delete']))) {
+		$is_read_only = isset($_GET['is_read_only']);
+		if (!$is_read_only && ($key1_owner == $_SESSION['username'] || $key1_owner == 'public'
+				|| $_SESSION['is_administrator'] && !is_administrator($s_key2)
+				|| $_SESSION['is_root']) && isset($_GET['delete'])) {
 			if (isset($_GET['confirm'])) {
-				pg_free_result(pgquery("DELETE FROM table_user WHERE tablename = $s_key1
-						AND username = $s_key2;"));
+				pgquery("DELETE FROM table_user WHERE tablename = $s_key1
+						AND username = $s_key2;");
 				echo "Row ($h_key1, $h_key2, FALSE) deleted.<br/>\n";
 			} else {
 ?>
@@ -106,7 +98,7 @@ if ($can_edit_permissions) {
 				(SELECT is_administrator FROM users WHERE username = u), EXISTS(SELECT TRUE
 				FROM table_user WHERE tablename = t AND username = 'public'
 				OR username = {$_SESSION['username']}) AS e FROM table_user
-				WHERE u = 'public' OR u = {$_SESSION['s_username']} OR NOT is_administrator OR e
+				WHERE NOT is_administrator OR e
 				ORDER BY is_administrator DESC, u ASC, t ASC, is_read_only ASC, username ASC;");
 		echo "Viewing table &quot;table_user&quot; for public, username {$_SESSION['h2username']}
 				and non-administrators.<br/>\n";
@@ -141,19 +133,10 @@ if ($can_edit_permissions) {
 						<input form="insert" type="text" name="tablename"/>
 					</td>
 					<td>
-<?php
-						if ($_SESSION['is_administrator']) {
-?>
-							<input form="insert" type="text" name="username"/>
-<?php
-						} else {
-							echo "<input type=\"text\" value=\"{$_SESSION['h1username']}\"
-									disabled=\"disabled\"/>\n";
-							echo "<input form=\"insert\" type=\"hidden\" name=\"username\"
-									value=\"{$_SESSION['h1username']}\"/>\n";
-						}
-?>
-						<input form="insert" type="checkbox" disabled="disabled"/>
+						<input form="insert" type="text" name="username"/>
+					</td>
+					<td>
+						<input type="checkbox" disabled="disabled"/>
 					</td>
 					<td>
 						<form id="insert" action="" method="GET">
@@ -187,15 +170,14 @@ if ($can_edit_permissions) {
 					</td>
 					<td>
 <?php
-						if ($_SESSION['is_administrator']) {
-							echo "<input form=$form type=\"text\" name=\"username\"
-									value=\"$username\"/>\n";
-						} else {
-							echo "<input type=\"text\" value=\"$username\"
-									disabled=\"disabled\"/>\n";
-							echo "<input form=$form type=\"hidden\" name=\"username\"
-									value=\"$username\" disabled=\"/>\n";
-						}
+						echo "<input form=$form type=\"text\" name=\"username\"
+								value=\"$username\"/>\n";
+?>
+					</td>
+					<td>
+<?php
+						echo "<input form=$form type=\"checkbox\" name=\"is_read_only\"",
+								$row[2] == 't' ? ' checked="checked"' : '', "/>\n";
 ?>
 					</td>
 <?php
