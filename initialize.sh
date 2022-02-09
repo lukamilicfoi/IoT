2da278725df67594c47206a95999fa9596e96e1d
Luka Milić <luka.milic@foi.hr>
2022-02-09 19:42:04 +0100

    Commi2

--- initialize.sh
+++ initialize.sh
@@ -2,9 +2,9 @@
 pg_ctl initdb -D data -o "-E UTF-8 -U postgres"
 sudo mkdir -m 777 /run/postgresql
 pg_ctl -D data start
+./init_db.sh
 sudo ./IoT &
 sleep 10
 sudo killall -s SIGINT IoT
-./init_db.sh
 pg_ctl -D data stop
 sudo cp -r http /srv
