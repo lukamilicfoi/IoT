da8d7f1e7a88673d338cbc4cc3bd50027b84688b
Luka Milić <luka.milic@foi.hr>
2022-05-02 14:01:57 +0200

    Commit2

--- http/view_users.php
+++ http/view_users.php
@@ -33,7 +33,7 @@ if ($can_edit_others) {
 		}
 	} elseif (!vacuous($_POST['username'])) {
 		$s_username = pg_escape_literal($_POST['username']);
-		$s_role = pgescaperole1($_POST['username']);
+		$role = pgescaperole1($_POST['username']);
 		$h_username = '&apos;' . htmlspecialchars($_POST['username']) . '&apos;';
 		if (($_SESSION['is_administrator'] && !isset($_POST['is_administrator'])
 				|| $_SESSION['is_root']) && isset($_POST['insert'])) {
@@ -50,16 +50,16 @@ if ($can_edit_others) {
 					use_lan_switch_algorithm, nsecs_id, nsecs_src, trust_sending, trust_receving,
 					default_gateway, my_eui, insecure_port, secure_port FROM configuration
 					WHERE username = {$_SESSION['s_username']};");
-			pgquery("CREATE ROLE $s_role;");
-			pgquery("GRANT CREATE ON SCHEMA public TO $s_role;");
+			pgquery("CREATE ROLE $role;");
+			pgquery("GRANT CREATE ON SCHEMA public TO $role;");
 			echo "User $h_username inserted.<br/>\n";
 		} elseif (!vacuous($_POST['key'])) {
 			$s_key = pg_escape_literal($_POST['key']);
-			$s_kole = pgescaperole1($_POST['username']);
+			$kole = pgescaperole1($_POST['username']);
 			$h_key = htmlspecialchars($_POST['key']);
-			if (($_SESSION['is_administrator'] && !isset($_POST['is_administrator'])
-					&& !is_administrator($s1username) || $_SESSION['is_root'])
-					&& isset($_POST['update1']) && ($_POST['key'] != 'root'
+			if ($_POST['key'] == 'public' || ($_SESSION['is_administrator']
+					&& !isset($_POST['is_administrator']) && !is_administrator($s_username)
+					|| $_SESSION['is_root']) && isset($_POST['update1']) && ($_POST['key'] != 'root'
 					&& $_POST['key'] != 'public' || $_POST['key'] == $_POST['username'])) {
 				$query = 'UPDATE users SET (username' . (!vacuous($_POST['password']) ? ', password'
 						: '') . ", is_administrator, $user_fields_joined, can_actually_login)
@@ -71,24 +71,24 @@ if ($can_edit_others) {
 				}
 				pgquery($query . formescapebool($_POST['can_actually_login'])
 						. ") WHERE username = $s_key;");
-				if ($s_kole != $s_role) {
-					pgquery("ALTER ROLE $s_kole RENAME TO $s_role;");
+				if ($kole != $role) {
+					pgquery("ALTER ROLE $kole RENAME TO $role;");
 				}
 				echo "User $h_key updated.<br/>\n";
 			}
 		}
 	} elseif (!vacuous($_GET['key'])) {
 		$s_key = pg_escape_literal($_GET['key']);
-		$s_kole = pgescaperole1($_GET['key']);
+		$kole = pgescaperole1($_GET['key']);
 		$h_key = '&apos;' . htmlspecialchars($_GET['key']) . '&apos;';
 		$u_key = urlencode($_GET['key']);
 		if (($_SESSION['is_administrator'] && !is_administrator($s_key) || $_SESSION['is_root'])
 				&& isset($_GET['delete']) && $_GET['key'] != 'root' && $_GET['key'] != 'public') {
 			if (isset($_GET['confirm'])) {
 				pgquery("DELETE FROM users WHERE username = $s_key;");
-				pgquery("DROP OWNED BY $s_kole CASCADE;");
+				pgquery("DROP OWNED BY $kole CASCADE;");
 				pgquery("DELETE FROM table_owner WHERE username = $s_key;");
-				pgquery("DROP ROLE $s_kole;");
+				pgquery("DROP ROLE $kole;");
 				echo "User $h_key deleted.<br/>\n";
 			} else {
 ?>
@@ -105,14 +105,14 @@ if ($can_edit_others) {
 }
 if ($can_edit_yourself && isset($_POST['update2']) && !vacuous($_POST['username'])) {
 	$s_username = pg_escape_literal($_POST['username']);
-	$s_role = pgescaperole1($_POST['username']);
+	$role = pgescaperole1($_POST['username']);
 	$h_username = '&apos;' . htmlspecialchars($_POST['username']) . '&apos;';
 	pgquery('UPDATE users SET (username' . (!vacuous($_POST['password']) ? ', password' : '')
 			. ') = ROW(' . pg_escape_literal($_POST['username']) . (!vacuous($_POST['password'])
 			? ', \'' . password_hash($_POST['password'], PASSWORD_DEFAULT) . '\'' : '')
 			. ") WHERE username = {$_SESSION['s_username']};");
-	if ($_SESSION['s1role'] != $s_role) {
-		pgquery("ALTER ROLE {$_SESSION['s1role']} RENAME TO $s_role;");
+	if ($_SESSION['s1role'] != $role) {
+		pgquery("ALTER ROLE {$_SESSION['s1role']} RENAME TO $role;");
 	}
 	echo "User $h_username updated.<br/>\n";
 }
@@ -242,7 +242,7 @@ if ($can_view_yourself || $can_view_others) {
 						} elseif ($username != $_SESSION['h1username'] && $can_edit_others) {
 							echo "<input form=\"update1_$username\" type=\"text\"
 									name=\"username\" value=\"$username\" ", $username == 'public'
-									? 'readonly' : 'required', "/>\n";
+									? 'readonly' : 'required', " autofocus/>\n";
 						} else {
 							echo "<input type=\"text\" value=\"$username\" disabled/>\n";
 						}
@@ -272,7 +272,7 @@ if ($can_view_yourself || $can_view_others) {
 									name=\"is_administrator\"",
 									$row[2] == 't' ? ' checked' : '', "/>\n";
 						} else {
-							echo "<input type=\"checkbox\"", $row[2] == 't' ? ' checked' : '',
+							echo '<input type="checkbox"', $row[2] == 't' ? ' checked' : '',
 									" disabled/>\n";
 						}
 ?>
@@ -320,7 +320,8 @@ if ($can_view_yourself || $can_view_others) {
 ?>
 								<form action="?" method="GET">
 <?php
-									echo "<input type=\"hidden\" name=\"key\" value=\"$username\"/>\n";
+									echo "<input type=\"hidden\" name=\"key\"
+											value=\"$username\"/>\n";
 ?>
 									<input type="submit" name="delete" value="DELETE"/>
 								</form>
